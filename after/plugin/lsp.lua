f97c99286bd97c7524b1ad0981ff95e55e95d338
edu <edu@hydra-Laptop-Windows.>
2024-02-29 11:37:44 -0300

    create new branch

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,7 +10,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
 	-- Replace the language servers listed here 
 	-- with the ones you want to install
-	ensure_installed = {'rust_analyzer', 'clangd', 'cmake', 'gopls', 'jdtls', 'asm_lsp'},
+	ensure_installed = {'clangd', 'cmake', 'jdtls'},
 	handlers = {
 		lsp.default_setup,
 	},
