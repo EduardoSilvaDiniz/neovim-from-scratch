c698c6b7c75f3094f6cd11d629a670b503baa213
edu <edu@DESKTOP-9GJQ8HR.localdomain>
2024-03-29 13:56:04 -0300

    add script install nvim

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/edu/.cache/nvim/packer_hererocks/2.1.1693350652/share/lua/5.1/?.lua;/home/edu/.cache/nvim/packer_hererocks/2.1.1693350652/share/lua/5.1/?/init.lua;/home/edu/.cache/nvim/packer_hererocks/2.1.1693350652/lib/luarocks/rocks-5.1/?.lua;/home/edu/.cache/nvim/packer_hererocks/2.1.1693350652/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/edu/.cache/nvim/packer_hererocks/2.1.1693350652/lib/lua/5.1/?.so"
+local package_path_str = "/home/edu/.cache/nvim/packer_hererocks/2.1.1707061634/share/lua/5.1/?.lua;/home/edu/.cache/nvim/packer_hererocks/2.1.1707061634/share/lua/5.1/?/init.lua;/home/edu/.cache/nvim/packer_hererocks/2.1.1707061634/lib/luarocks/rocks-5.1/?.lua;/home/edu/.cache/nvim/packer_hererocks/2.1.1707061634/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/edu/.cache/nvim/packer_hererocks/2.1.1707061634/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
---
+++ startup_opensuse.sh
@@ -0,0 +1,31 @@
+PACKAGER='zypper'
+LIST_PACKAGES='zsh neovim cargo rust python3 openjdk go git openssl-devel'
+META_PACKAGES='devel_basis'
+
+sudo $PACKAGER install $LIST_PACKAGES -y
+sudo $PACKAGER install -t pattern $META_PACKAGES -y
+
+git clone https://github.com/EduardoSilvaDiniz/neovim-vanilla.git ~/.config/nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/sta
+rt/packer.nvim
+
+# installs NVM (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+# download and install Node.js
+nvm install 20
+# verifies the right Node.js version is in the environment
+node -v # should print `v20.12.0`
+# verifies the right NPM version is in the environment
+npm -v # should print `10.5.0`
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "
+alias vi='nvim'
+alias vim='nvim'
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+" >> .zshrc
+
+echo "Abra nvim e execute PackerSync para instalar os plugins"
