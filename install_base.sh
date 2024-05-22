54cfd860ece0e5b08f5551c9391f13a650f309c9
Eduardo Silva <122567746+EduardoSilvaDiniz@users.noreply.github.com>
2024-05-22 10:39:32 -0300

    Update install_base.sh

--- install_base.sh
+++ install_base.sh
@@ -2,13 +2,6 @@
 sudo apt update && sudo apt upgrade -y && \
   sudo apt install neofetch zsh neovim cargo git unzip zip build-essential -y
 
-# clone my repository nvim configs
-git clone https://github.com/EduardoSilvaDiniz/neovim-vanilla.git ~/.config/nvim
-
-# install packer
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
