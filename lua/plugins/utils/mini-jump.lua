edbeb62fa2400624d31fa60716f3c6da9268ab72
Eduardo Silva <eduardofox989@protonmail.com>
2024-09-10 14:27:38 -0300

    disable autocmd because it was causing problems in dart and adding mini.jump and mini.jump2d to improve navigation

---
+++ lua/plugins/utils/mini-jump.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"echasnovski/mini.jump",
+		version = "*",
+		config = function()
+			require("mini.jump").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.jump2d",
+		version = "*",
+		config = function()
+			require("mini.jump2d").setup()
+		end,
+	},
+}
