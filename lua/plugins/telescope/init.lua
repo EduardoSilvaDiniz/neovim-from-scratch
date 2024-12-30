a1dbfa28a7dac02bee0eeeffc131fa1cb9d9e254
Eduardo Silva <eduardofox989@protonmail.com>
2024-12-30 16:28:41 -0300

    add plugin telescope

---
+++ lua/plugins/telescope/init.lua
@@ -0,0 +1,17 @@
+return {
+	"nvim-telescope/telescope.nvim",
+	event = "VimEnter",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope-ui-select.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			cond = function()
+				return vim.fn.executable("make") == 1
+			end,
+		},
+		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+	},
+}
