3e400154f69423d2bcfa1c62a7dd9bdd10037bbc
Eduardo Silva <eduardofox989@protonmail.com>
2024-12-30 17:17:18 -0300

    enable nerd font

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
+vim.g.have_nerd_font = true
 
 vim.keymap.set("n", "<leader>tn", "<cmd>Neotree filesystem reveal left<cr>", { desc = "Toggle tree popup" })
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Format buffer" })
