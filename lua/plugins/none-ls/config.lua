3babce5edf800922e5b44dced948a35acaf09113
Eduardo Silva <eduardofox989@protonmail.com>
2024-12-30 18:06:40 -0300

    update none-ls add format and diag for c/c++, java, nix, lua

--- lua/plugins/none-ls/config.lua
+++ lua/plugins/none-ls/config.lua
@@ -1,8 +1,25 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-  sources = {
-    -- Lua
-    null_ls.builtins.formatting.stylua,
-  },
+	sources = {
+		-- C/C++
+		null_ls.builtins.formatting.clang_format,
+		null_ls.builtins.diagnostics.cppcheck,
+
+		-- Editorconfig
+    null_ls.builtins.diagnostics.editorconfig_checker,
+
+		-- Java
+		null_ls.builtins.formatting.google_java_format,
+		null_ls.builtins.diagnostics.checkstyle,
+		null_ls.builtins.diagnostics.pmd,
+
+		-- Nix
+		null_ls.builtins.formatting.alejandra,
+		null_ls.builtins.code_actions.statix,
+		null_ls.builtins.diagnostics.deadnix,
+
+		-- Lua
+		null_ls.builtins.formatting.stylua,
+	},
 })
