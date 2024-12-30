507ca06b7ee0d42586e6d4e1ffa41f9ae34876d6
Eduardo Silva <eduardofox989@protonmail.com>
2024-12-30 18:08:14 -0300

    add clangd

--- lua/plugins/lspconfig/config.lua
+++ lua/plugins/lspconfig/config.lua
@@ -3,6 +3,7 @@ local lsp = require("lspconfig")
 local servers = {
 	lua_ls = require("lsp.config.lua_ls"),
 	nil_ls = require("lsp.config.nil_ls"),
+	clangd = require("lsp.config.clangd"),
 }
 
 for name, conf in pairs(servers) do
