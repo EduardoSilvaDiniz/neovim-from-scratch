529b32b6206cdc42247147a28380c9295a7c6c3d
Eduardo Silva <eduardofox989@protonmail.com>
2025-01-11 20:16:14 -0300

    move load keys to init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require("core.lazy")
 require("core.options")
 require("core.autocmd")
+require("core.keymaps").load_plugins()
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -3,6 +3,3 @@ vim.g.maplocalleader = " "
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.g.have_nerd_font = true
-
-local kys = require("core.keymaps")
-kys.load_plugins()
