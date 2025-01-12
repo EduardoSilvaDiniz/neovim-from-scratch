78730f24a99b3a669772f3139f74f562bf7f77c4
Eduardo Silva <eduardofox989@protonmail.com>
2025-01-11 21:30:26 -0300

    move keymaps to keymaps table

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -2,15 +2,6 @@ local config = {
 	cmd = { "/nix/store/014m3wvsnfskhpcxqk58qjnpsvrlsg20-user-environment/bin/jdtls" },
 	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
 }
-local jdtls = require("jdtls")
 
-vim.keymap.set("n", "<A-o>", jdtls.organize_imports, { desc = "Organize imports" })
-vim.keymap.set("n", "<leader>cra", jdtls.extract_variable, { desc = "Extract variable" })
-vim.keymap.set("n", "<leader>crd", jdtls.extract_constant, { desc = "Extract constant" })
-vim.keymap.set("v", "<leader>crm", jdtls.extract_method, { desc = "Extract method" })
-
--- nvim-dap mappings (requires java-debug and vscode-java-test bundles)
-vim.keymap.set("n", "<leader>df", jdtls.test_class, { desc = "Run test class" })
-vim.keymap.set("n", "<leader>dn", jdtls.test_nearest_method, { desc = "Run nearest test method" })
-
+require("core.keymaps").manual_load("jdtls")
 require("jdtls").start_or_attach(config)
