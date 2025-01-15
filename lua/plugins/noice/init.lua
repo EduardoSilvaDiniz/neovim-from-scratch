0bf317986c10a6ccb9d4666a85021dcd5f8e5700
Eduardo Silva <eduardofox989@protonmail.com>
2025-01-14 23:00:46 -0300

    add noice plugin and configuration

---
+++ lua/plugins/noice/init.lua
@@ -0,0 +1,18 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		{
+			"rcarriga/nvim-notify",
+			config = function()
+				vim.notify = require("notify")
+				require("notify").setup({
+					render = "wrapped-compact",
+					stages = "static",
+				})
+			end,
+		},
+	},
+	opts = require("plugins.noice.config"),
+}
