b4925a93ff4119346f305f80d7b773031f5fa369
Eduardo Silva <eduardofox989@protonmail.com>
2025-01-14 23:03:00 -0300

    active compatibility with plugin nui

--- lua/plugins/dressing/config.lua
+++ lua/plugins/dressing/config.lua
@@ -1,7 +1,13 @@
 return {
+	select = {
+		backend = { "nui" },
+		nui = {
+			min_width = 7,
+			min_height = 2,
+		},
+	},
 	input = {
 		relative = "win",
-
 		mappings = {
 			n = {
 				["<Esc>"] = "Close",
