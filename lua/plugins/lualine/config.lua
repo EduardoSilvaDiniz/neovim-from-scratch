40f48305c78fe3b9c765a2ac2aa9650a2a577dd5
Eduardo Silva <eduardofox989@protonmail.com>
2025-01-14 22:26:23 -0300

    add config to disable lualine on neotree

--- lua/plugins/lualine/config.lua
+++ lua/plugins/lualine/config.lua
@@ -4,10 +4,7 @@ require("lualine").setup({
 		theme = "auto",
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
-		disabled_filetypes = {
-			statusline = {},
-			winbar = {},
-		},
+		disabled_filetypes = { "packer", "NVimTree", "neo-tree" },
 		ignore_focus = {},
 		always_divide_middle = true,
 		always_show_tabline = true,
