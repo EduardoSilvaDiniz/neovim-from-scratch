402467fab68ef5ba56a8e664f8b64d8c91f0c2bb
Eduardo Silva <eduardofox989@protonmail.com>
2025-01-14 21:50:44 -0300

    add key back close folder change key to open file add event auto close on open file

--- lua/plugins/neo-tree/config.lua
+++ lua/plugins/neo-tree/config.lua
@@ -1,9 +1,29 @@
 return {
+	event_handlers = {
+		{
+			event = "file_open_requested",
+			handler = function()
+				require("neo-tree.command").execute({ action = "close" })
+			end,
+		},
+	},
 	filesystem = {
 		window = {
 			mappings = {
 				["\\"] = "close_window",
+				["l"] = "open",
+				["h"] = "back_close_folder",
 			},
 		},
+	},
+	commands = {
+		back_close_folder = function(state)
+			local node = state.tree:get_node()
+			if node.type == "directory" and node:is_expanded() then
+				state.commands.toggle_node(state)
+			else
+				require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+			end
+		end,
 	},
 }
