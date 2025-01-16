d3f3e877987d5999ce8335271560ba894467d103
Eduardo Silva <eduardofox989@protonmail.com>
2025-01-16 13:51:41 -0300

    add neovim-project plugin

---
+++ lua/plugins/neovim-project/init.lua
@@ -0,0 +1,12 @@
+return {
+	"coffebar/neovim-project",
+	lazy = false,
+	priority = 100,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope.nvim",
+		"Shatur/neovim-session-manager",
+		"ibhagwan/fzf-lua",
+	},
+	opts = require("plugins.project.config"),
+}
