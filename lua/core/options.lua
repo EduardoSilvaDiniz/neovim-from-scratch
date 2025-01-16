333e1c5a742a7bae6d125ebeac02663686b5db0f
Eduardo Silva <eduardofox989@protonmail.com>
2025-01-16 15:54:54 -0300

    add keys to quickquit and restore last session

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -3,3 +3,7 @@ vim.g.maplocalleader = " "
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.g.have_nerd_font = true
+
+vim.keymap.set("n", "<leader>qq", "<cmd>q<cr>", { desc = "Quit neovim" })
+vim.keymap.set("n", "<leader>qs", "<cmd>q!<cr>", { desc = "Quit neovim not saving" })
+vim.keymap.set("n", "<leader>ql", "<cmd>NeovimProjectLoadRecent<cr>", { desc = "Restore last session" })
