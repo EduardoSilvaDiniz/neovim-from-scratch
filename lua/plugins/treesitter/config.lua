314d1df31372571cf2b21b372577468f79b62f7b
Eduardo Silva <eduardofox989@protonmail.com>
2025-01-16 15:44:47 -0300

    add new configs to treesitter

--- lua/plugins/treesitter/config.lua
+++ lua/plugins/treesitter/config.lua
@@ -1,11 +1,16 @@
 return {
-	modules = {},
-	ensure_installed = {},
+	ensure_installed = "",
 	sync_install = true,
 	auto_install = true,
+	TSUpdate = true,
 	ignore_install = {},
 	highlight = {
 		enable = true,
-		additional_vim_regex_highlighting = false,
+		additional_vim_regex_highlighting = true,
 	},
+	indent = {
+		enable = true,
+	},
+	modules = {},
+	parser_install_dir = nil,
 }
