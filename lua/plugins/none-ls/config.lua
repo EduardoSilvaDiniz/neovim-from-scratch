6e584d4654369b45a2120b9669d64be84c52c039
Eduardo Silva <eduardofox989@protonmail.com>
2025-01-16 18:07:11 -0300

    add goimports

--- lua/plugins/none-ls/config.lua
+++ lua/plugins/none-ls/config.lua
@@ -18,6 +18,7 @@ return function(_, opts)
 		-- Go
 		nls.builtins.formatting.gofumpt,
 		nls.builtins.formatting.golines,
+		nls.builtins.formatting.goimports,
 		nls.builtins.formatting.goimports_reviser,
 		nls.builtins.diagnostics.golangci_lint,
 		nls.builtins.code_actions.gomodifytags,
