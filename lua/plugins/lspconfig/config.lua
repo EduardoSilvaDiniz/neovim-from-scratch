b6c8a6c8e74e57fc2a588b085e0fb30d97b39fb9
Eduardo Silva <eduardofox989@protonmail.com>
2025-01-16 15:45:03 -0300

    add golps on lspconfig

--- lua/plugins/lspconfig/config.lua
+++ lua/plugins/lspconfig/config.lua
@@ -4,6 +4,7 @@ local servers = {
 	lua_ls = require("lsp.config.lua_ls"),
 	nil_ls = require("lsp.config.nil_ls"),
 	clangd = require("lsp.config.clangd"),
+	gopls = require("lsp.config.gopls"),
 }
 
 for name, conf in pairs(servers) do
