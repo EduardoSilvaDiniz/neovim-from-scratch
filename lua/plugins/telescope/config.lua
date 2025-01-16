e0b5e5d675014e4126dad29e086aff04f7bcb489
Eduardo Silva <eduardofox989@protonmail.com>
2025-01-16 15:14:17 -0300

    change function key ESC on telescope

--- lua/plugins/telescope/config.lua
+++ lua/plugins/telescope/config.lua
@@ -1,4 +1,13 @@
+local actions = require("telescope.actions")
+
 return {
+	defaults = {
+		mappings = {
+			i = {
+				["<esc>"] = actions.close,
+			},
+		},
+	},
 	extensions = {
 		["ui-select"] = {
 			require("telescope.themes").get_dropdown(),
