fa6b233914ce5a7ad31036a737ae327e3226b0fb
Eduardo Silva <eduardofox989@protonmail.com>
2025-01-19 13:02:38 -0300

    delete jdtls lsp

--- ftplugin/java.lua
+++
@@ -1,7 +0,0 @@
-local config = {
-	cmd = { "/nix/store/014m3wvsnfskhpcxqk58qjnpsvrlsg20-user-environment/bin/jdtls" },
-	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
-}
-
-require("core.keymaps").manual_load("jdtls")
-require("jdtls").start_or_attach(config)
