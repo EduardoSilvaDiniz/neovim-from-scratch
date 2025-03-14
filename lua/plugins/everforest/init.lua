1f8648b46a8252c83002ad3440e9fbf245a6a9ea
Eduardo Silva <eduardofox989@protonmail.com>
2025-03-14 14:23:46 -0300

    fix color theme

--- lua/plugins/everforest/init.lua
+++ lua/plugins/everforest/init.lua
@@ -4,6 +4,8 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
+		-- vim.api.nvim_set_hl(0, "FloatBorder", {fg = normal, bg = normal})
+		-- vim.api.nvim_set_hl(0, "NormalFloat", {fg = normal, bg = normal})
 		vim.cmd.colorscheme("everforest")
 	end,
 }
