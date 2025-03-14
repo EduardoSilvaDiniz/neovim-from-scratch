a17d3f964220ba363e4f3553c772024ab981866e
Eduardo Silva <eduardofox989@protonmail.com>
2025-03-14 14:30:38 -0300

    add news keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,6 +2,7 @@ pcall(require("telescope").load_extension, "fzf")
 pcall(require("telescope").load_extension, "ui-select")
 local substitute = require("substitute")
 local builtin = require("telescope.builtin")
+local refactoring = require("refactoring")
 local M = {}
 
 local function check_plugin(plugin)
@@ -16,16 +17,16 @@ end
 
 local keys_auto = {
 	telescope = {
-		{ "<leader>sh",       builtin.help_tags,   { desc = "[S]earch [H]elp" } },
-		{ "<leader>sk",       builtin.keymaps,     { desc = "[S]earch [K]eymaps" } },
-		{ "<leader>sf",       builtin.find_files,  { desc = "[S]earch [F]iles" } },
-		{ "<leader>ss",       builtin.builtin,     { desc = "[S]earch [S]elect Telescope" } },
-		{ "<leader>sw",       builtin.grep_string, { desc = "[S]earch current [W]ord" } },
-		{ "<leader>sg",       builtin.live_grep,   { desc = "[S]earch by [G]rep" } },
-		{ "<leader>sd",       builtin.diagnostics, { desc = "[S]earch [D]iagnostics" } },
-		{ "<leader>sr",       builtin.resume,      { desc = "[S]earch [R]esume" } },
-		{ "<leader>s.",       builtin.oldfiles,    { desc = '[S]earch Recent Files ("." for repeat)' } },
-		{ "<leader><leader>", builtin.buffers,     { desc = "[ ] Find existing buffers" } },
+		{ "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" } },
+		{ "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" } },
+		{ "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" } },
+		{ "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" } },
+		{ "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" } },
+		{ "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" } },
+		{ "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" } },
+		{ "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" } },
+		{ "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' } },
+		{ "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" } },
 		{
 			"<leader>/",
 			function()
@@ -55,25 +56,34 @@ local keys_auto = {
 		},
 	},
 	substitute = {
-		{ "s",  substitute.operator, { noremap = true } },
-		{ "ss", substitute.line,     { noremap = true } },
-		{ "S",  substitute.eol,      { noremap = true } },
-		{ "s",  substitute.visual,   { noremap = true }, { "x" } },
+		{ "s", substitute.operator, { noremap = true } },
+		{ "ss", substitute.line, { noremap = true } },
+		{ "S", substitute.eol, { noremap = true } },
+		{ "s", substitute.visual, { noremap = true }, { "x" } },
+	},
+	refactoring = {
+		{ "<leader>re",  "<cmd>Refactor extract<cr>", {desc = "extract"}, "x" },
+		{ "<leader>rf",  "<cmd>Refactor extract_to_file<cr>", {desc = "extract to file"},"x" },
+		{ "<leader>rv",  "<cmd>Refactor extract_var<cr>", {desc = "extract var"},"x" },
+		{ "<leader>ri",  "<cmd>Refactor inline_var<cr>", {desc = "inline var"},{ "n", "x" } },
+		{ "<leader>rI",  "<cmd>Refactor inline_func<cr>", {desc = "inline_func"}},
+		{ "<leader>rb",  "<cmd>Refactor extract_block<cr>", {desc = "extract_block"} },
+		{ "<leader>rbf", "<cmd>Refactor extract_block_to_file<cr>", {desc = "extract_block_to_file"}},
 	},
 }
 
 local keys_manual = {
 	lspconfig = {
-		{ "gd",         builtin.lsp_definitions,               { desc = "[G]oto [D]efinition" } },
-		{ "gr",         builtin.lsp_references,                { desc = "[G]oto [R]eferences" } },
-		{ "gI",         builtin.lsp_implementations,           { desc = "[G]oto [I]mplementation" } },
-		{ "<leader>D",  builtin.lsp_type_definitions,          { desc = "Type [D]efinition" } },
-		{ "<leader>ds", builtin.lsp_document_symbols,          { desc = "[D]ocument [S]ymbols" } },
+		{ "gd", builtin.lsp_definitions, { desc = "[G]oto [D]efinition" } },
+		{ "gr", builtin.lsp_references, { desc = "[G]oto [R]eferences" } },
+		{ "gI", builtin.lsp_implementations, { desc = "[G]oto [I]mplementation" } },
+		{ "<leader>D", builtin.lsp_type_definitions, { desc = "Type [D]efinition" } },
+		{ "<leader>ds", builtin.lsp_document_symbols, { desc = "[D]ocument [S]ymbols" } },
 		{ "<leader>ws", builtin.lsp_dynamic_workspace_symbols, { desc = "[W]orkspace [S]ymbols" } },
-		{ "<leader>rn", vim.lsp.buf.rename,                    { desc = "[R]e[n]ame" } },
-		{ "<leader>ca", vim.lsp.buf.code_action,               { desc = "[C]ode [A]ction" },        { "n", "x" } },
-		{ "gD",         vim.lsp.buf.declaration,               { desc = "[G]oto [D]eclaration" } },
-		{ "<leader>f",  vim.lsp.buf.format,                    { desc = "Format buffer " } },
+		{ "<leader>rn", vim.lsp.buf.rename, { desc = "[R]e[n]ame" } },
+		{ "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ction" }, { "n", "x" } },
+		{ "gD", vim.lsp.buf.declaration, { desc = "[G]oto [D]eclaration" } },
+		{ "<leader>f", vim.lsp.buf.format, { desc = "Format buffer " } },
 	},
 
 	lazygit = {
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -3,7 +3,15 @@ vim.g.maplocalleader = " "
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.g.have_nerd_font = true
+vim.wo.number = true
+
+local augroup = vim.api.nvim_create_augroup("user_cmds", { clear = true })
+local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<leader>qq", "<cmd>q<cr>", { desc = "Quit neovim" })
 vim.keymap.set("n", "<leader>qs", "<cmd>q!<cr>", { desc = "Quit neovim not saving" })
 vim.keymap.set("n", "<leader>ql", "<cmd>NeovimProjectLoadRecent<cr>", { desc = "Restore last session" })
+vim.keymap.set("n", "tn", "<cmd>Neotree reveal<cr>", { noremap = true })
+vim.keymap.set("n", "<leader>pp", "<cmd>Telescope neovim-project discover<cr>", { desc = "Switch project" })
+vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Find file in project" })
+vim.keymap.set("n", "<leader>pr", "<cmd>Telescope frecency<cr>", { desc = "Find recent project files" })
