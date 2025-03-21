f85ba19ef3a6b78931589c2f7f4b9196bc266304
Eduardo Silva <eduardofox989@protonmail.com>
2025-03-21 15:51:05 -0300

    remove: unnecessary null-ls

--- lua/plugins/none-ls/config.lua
+++ lua/plugins/none-ls/config.lua
@@ -3,26 +3,6 @@ return function(_, opts)
 	opts.root_dir = opts.root_dir
 			or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
 	opts.sources = vim.list_extend(opts.sources or {}, {
-		-- C/C++
-		nls.builtins.formatting.clang_format,
-		nls.builtins.diagnostics.cppcheck,
-
-		-- Editorconfig
-		nls.builtins.diagnostics.editorconfig_checker,
-
-		-- Java
-		nls.builtins.formatting.google_java_format,
-		nls.builtins.diagnostics.checkstyle,
-		nls.builtins.diagnostics.pmd,
-
-		-- Go
-		nls.builtins.formatting.gofumpt,
-		nls.builtins.formatting.golines,
-		nls.builtins.formatting.goimports,
-		nls.builtins.formatting.goimports_reviser,
-		nls.builtins.diagnostics.golangci_lint,
-		nls.builtins.code_actions.gomodifytags,
-
 		-- Nix
 		nls.builtins.formatting.alejandra,
 		nls.builtins.code_actions.statix,
@@ -30,5 +10,9 @@ return function(_, opts)
 
 		-- Lua
 		nls.builtins.formatting.stylua,
+
+		-- Typescript
+		nls.builtins.formatting.prettier,
+		nls.builtins.code_actions.refactoring
 	})
 end
