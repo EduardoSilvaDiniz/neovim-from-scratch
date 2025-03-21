19227d1234bda9661a0ff901731d0b493d1c6ee7
Eduardo Silva <eduardofox989@protonmail.com>
2025-03-21 15:37:52 -0300

    enable inlay_hints

--- lua/plugins/lspconfig/init.lua
+++ lua/plugins/lspconfig/init.lua
@@ -2,6 +2,9 @@ return {
 	"neovim/nvim-lspconfig",
 	cmd = { "LspInfo", "LspInstall", "LspStart" },
 	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		inlay_hints = { enable = true },
+	},
 	config = function()
 		require("plugins.lspconfig.config")
 	end,
