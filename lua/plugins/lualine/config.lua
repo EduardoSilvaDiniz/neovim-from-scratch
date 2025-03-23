ec2dc234cc242e481a488724e9b0aa348c3cc028
Eduardo Silva <eduardofox989@protonmail.com>
2025-03-22 23:05:33 -0300

    refactoring: lualine configuration simplified and bar more minimalist

--- lua/plugins/lualine/config.lua
+++ lua/plugins/lualine/config.lua
@@ -1,67 +1,112 @@
-return {
+local config = {
 	options = {
 		icons_enabled = false,
-		theme = "auto",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
-		disabled_filetypes = { "neo-tree" },
-		ignore_focus = {},
-		always_divide_middle = true,
-		always_show_tabline = true,
-		globalstatus = false,
-		refresh = {
-			statusline = 100,
-			tabline = 100,
-			winbar = 100,
+		component_separators = "",
+		section_separators = "",
+		theme = {
+			normal = { c = { fg = "#ebdbb2", bg = "#282828" } },
+			inactive = { c = { fg = "#ebdbb2", bg = "#282828" } },
 		},
+		disabled_filetypes = { "neo-tree" },
 	},
 	sections = {
-		lualine_a = { { "filename", file_status = true, path = 1 } },
-		lualine_b = { "diagnostics" },
-		lualine_c = {
-			{
-				function()
-					local msg = "No Active Lsp"
-					local buf_ft = vim.api.nvim_get_option_value("filetype", { buf = 0 })
-					local clients = vim.lsp.get_clients()
-
-					if next(clients) == nil then
-						return msg
-					end
-
-					for _, client in ipairs(clients) do
-						if client.name == "null-ls" then
-							goto continue
-						end
-
-						local filetypes = client.config.filetypes
-						if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-							return client.name
-						end
-
-						::continue::
-					end
-
-					return msg
-				end,
-				icon = " LSP:",
-				color = { fg = "#ffffff", gui = "bold" },
-			},
-		},
-		lualine_x = { "branch", "diff", "diagnostics" },
-		lualine_y = { "location" },
-		lualine_z = { "mode" },
+		lualine_a = {},
+		lualine_b = {},
+		lualine_y = {},
+		lualine_z = {},
+		lualine_c = {},
+		lualine_x = {},
 	},
 	inactive_sections = {
 		lualine_a = {},
 		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
 		lualine_y = {},
 		lualine_z = {},
+		lualine_c = {},
+		lualine_x = {},
 	},
-	tabline = {},
-	winbar = {},
-	inactive_winbar = {},
-	extensions = {},
 }
+
+local function insert_left(component)
+	table.insert(config.sections.lualine_c, component)
+end
+
+local function insert_right(component)
+	table.insert(config.sections.lualine_x, component)
+end
+
+local function macro_recording()
+	local reg = vim.fn.reg_recording()
+	if reg ~= "" then
+		return "@" .. reg -- Ícone de gravação + nome do registro
+	end
+	return ""
+end
+
+insert_left({ macro_recording })
+
+insert_left({
+	"mode",
+})
+
+insert_left({
+	"filename",
+	file_status = true,
+	path = 1,
+})
+
+insert_left({
+	"location",
+})
+
+insert_right({
+	"branch",
+})
+
+insert_right({
+	"diff",
+	symbols = {
+		added = "",
+		modified = "",
+		removed = "",
+	},
+})
+
+insert_right({
+	function()
+		local msg = "No Active Lsp"
+		local buf_ft = vim.api.nvim_get_option_value("filetype", { buf = 0 })
+		local clients = vim.lsp.get_clients()
+
+		if next(clients) == nil then
+			return msg
+		end
+
+		for _, client in ipairs(clients) do
+			if client.name == "null-ls" then
+				goto continue
+			end
+
+			local filetypes = client.config.filetypes
+			if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+				return client.name
+			end
+
+			::continue::
+		end
+
+		return msg
+	end,
+})
+
+insert_right({
+	"diagnostics",
+	sources = { "nvim_diagnostic" },
+	sections = { "error", "warn", "hint" },
+	symbols = { error = "", warn = "", hint = "" },
+	colored = true,
+	update_in_insert = false,
+	always_visible = true,
+})
+
+return config
--- lua/plugins/lualine/init.lua
+++ lua/plugins/lualine/init.lua
@@ -1,5 +1,5 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = "nvim-tree/nvim-web-devicons",
-	opts = require("plugins.lualine.config")
+	opts = require("plugins.lualine.config"),
 }
