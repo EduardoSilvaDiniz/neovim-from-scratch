a40b880273b684e8823c1140acfd0f23da17c3cb
Eduardo Silva <eduardofox989@protonmail.com>
2025-03-22 23:08:16 -0300

    added: new plugin typescript-tools

---
+++ lua/plugins/typescript-tools/config.lua
@@ -0,0 +1,14 @@
+return {
+	settings = {
+		tsserver_file_preferences = {
+			includeInlayParameterNameHints = "all",
+			includeCompletionsForModuleExports = true,
+			quotePreference = "auto",
+		},
+		tsserver_format_options = {
+			allowIncompleteCompletions = false,
+			allowRenameOfImportPath = false,
+		},
+		tsserver_locale = "pt-br",
+	},
+}
---
+++ lua/plugins/typescript-tools/init.lua
@@ -0,0 +1,8 @@
+return {
+	"pmizio/typescript-tools.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"neovim/nvim-lspconfig",
+	},
+	opts = require("plugins.typescript-tools.config"),
+}
