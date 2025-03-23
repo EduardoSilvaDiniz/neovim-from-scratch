6e4989fe2ded2f3fa6ac90ebb60ac3659adc44a3
Eduardo Silva <eduardofox989@protonmail.com>
2025-03-22 23:07:27 -0300

    added: new plugin toggleterm
    keybind ctrl+' to open terminal

---
+++ lua/plugins/toggleterm/config.lua
@@ -0,0 +1,11 @@
+return {
+	size = 65,
+	open_mapping = [[<C-'>]],
+	hide_numbers = true,
+	shade_terminals = true,
+	shading_factor = 2,
+	start_in_insert = true,
+	direction = "vertical",
+	close_on_exit = true,
+	shell = vim.o.shell,
+}
---
+++ lua/plugins/toggleterm/init.lua
@@ -0,0 +1,5 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	opts = require("plugins.toggleterm.config"),
+}
