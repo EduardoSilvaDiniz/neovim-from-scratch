500ed39eb71b5845e8697607dbbb9793308eb031
Eduardo Silva <eduardofox989@protonmail.com>
2025-03-23 20:48:31 -0300

    improved: function that creates terminal and executes command
    checked if the user typed something, fixed toggle, solved focus problem
    and added keymap for shutdown in the terminal

--- lua/plugins/toggleterm/keymap.lua
+++ lua/plugins/toggleterm/keymap.lua
@@ -7,27 +7,48 @@ local quick_command = Terminal:new({
 	hidden = true,
 })
 
-local function _toggle_quick_command()
-	if quick_command:is_open() then
-		quick_command:toggle()
-	else
-		vim.ui.input({ prompt = "Comando: " }, function(input)
-			if input then
-				quick_command.cmd = input
-				quick_command:toggle()
-			end
-		end)
-	end
+local function get_focus()
+	vim.defer_fn(function()
+		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, false, true), "n", true)
+		vim.api.nvim_set_current_win(vim.fn.win_getid())
+	end, 100)
 end
 
-local function _rerun_last_command()
-	if quick_command.cmd ~= "" then
+--- abre um caixa de entrada para digitar um comando que será executando em um terminal
+--- caso o terminal já esteja aberto, será fechando e aberto outro
+local function run_command_terminal()
+	quick_command:shutdown()
+	vim.ui.input({ prompt = "Comando: " }, function(input)
+		if not input or input == "" then
+			vim.api.nvim_err_writeln("Por favor, forneça um comando para o terminal.")
+			return
+		end
+
+		quick_command.cmd = input
 		quick_command:toggle()
-	else
-		vim.notify("Nenhum comando executado ainda!", vim.log.levels.WARN)
-	end
+		get_focus()
+		vim.api.nvim_buf_set_keymap(0, "n", "Q", ":q!<CR>", { noremap = true, silent = true })
+	end)
 end
+
+--- altera o estado do terminal de minimizado e maximizado
+--- caso o terminal esteja fechando, ele executa com o ultimo comando
+local function toggle_terminal()
+	if quick_command.cmd == "" then
+		vim.api.nvim_err_writeln("Por favor, forneça um comando para o terminal.")
+		return
+	end
+	quick_command:toggle()
+	get_focus()
+	vim.api.nvim_buf_set_keymap(0, "n", "Q", ":q!<CR>", { noremap = true, silent = true })
+end
+
+local function shutdown_terminal()
+	quick_command:shutdown()
+end
+
 return {
-	{ "<leader>ce", _toggle_quick_command, { desc = "Abrir/Minimizar terminal" } },
-	{ "<leader>cr", _rerun_last_command,   { desc = "Reexecutar último comando" } },
+	{ "<leader>ce", run_command_terminal, { desc = "executar comando no terminal" } },
+	{ "<leader>tt", toggle_terminal,      { desc = "altera terminal" } },
+	{ "<leader>tc", shutdown_terminal,    { desc = "fecha o terminal" } },
 }
