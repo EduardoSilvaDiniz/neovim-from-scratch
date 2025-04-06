929e320e5e8136830afc6404efd7572bfafb7bb2
Eduardo Silva <eduardofox989@protonmail.com>
2025-04-06 00:13:16 -0300

    fix: depreciated function

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -7,21 +7,21 @@ vim.wo.number = true
 vim.lsp.inlay_hint.enable(true)
 vim.opt.signcolumn = "yes:1"
 vim.diagnostic.config({
-  virtual_text = false, -- Desativa mensagens inline
-  signs = true,         -- Mantém os ícones laterais
-  underline = true,     -- Mantém sublinhado
+	virtual_text = false, -- Desativa mensagens inline
+	signs = true,        -- Mantém os ícones laterais
+	underline = true,    -- Mantém sublinhado
 })
-local signs = {
-	Error = "",
-	Warn = "",
-	Hint = "",
-	Info = "",
-}
 
-for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+vim.diagnostic.config({
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "",
+			[vim.diagnostic.severity.WARN]  = "",
+			[vim.diagnostic.severity.INFO]  = "",
+			[vim.diagnostic.severity.HINT]  = "",
+		},
+	},
+})
 
 local normal_bg = vim.api.nvim_get_hl(0, { name = "Normal" }).bg
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = normal_bg })
