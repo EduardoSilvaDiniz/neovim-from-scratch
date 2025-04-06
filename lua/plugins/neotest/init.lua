ad8a29b2de66309e3ef78e12dd16892b875dfd21
Eduardo Silva <eduardofox989@protonmail.com>
2025-04-05 23:34:44 -0300

    added: neotest plugin

---
+++ lua/plugins/neotest/init.lua
@@ -0,0 +1,11 @@
+return {
+	"nvim-neotest/neotest",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"nvim-lua/plenary.nvim",
+		"antoinemadec/FixCursorHold.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-neotest/neotest-go",
+	},
+	config = require("plugins.neotest.config"),
+}
