0343b253f26c886e25996c0d5fd810fd5a5be69c
Eduardo Silva <eduardofox989@protonmail.com>
2025-04-07 19:26:20 -0300

    change: more formats have been placed within the check function

--- lua/plugins/none-ls/config.lua
+++ lua/plugins/none-ls/config.lua
@@ -18,12 +18,17 @@ return function(_, opts)
 		null_ls.builtins.formatting.clang_format,
 		null_ls.builtins.diagnostics.cppcheck,
 		null_ls.builtins.diagnostics.gccdiag,
+		-- Make
+		null_ls.builtins.diagnostics.checkmake,
+		null_ls.builtins.diagnostics.cmake_lint,
 	})
 
 	utils.add_formatter_if_lsp("jdtls", opts.sources, {
 		null_ls.builtins.formatting.google_java_format,
 		null_ls.builtins.diagnostics.checkstyle,
 		null_ls.builtins.diagnostics.pmd,
+		-- Gradle
+		null_ls.builtins.diagnostics.npm_groovy_lint,
 	})
 
 	utils.add_formatter_if_lsp("gopls", opts.sources, {
@@ -40,6 +45,79 @@ return function(_, opts)
 		null_ls.builtins.formatting.stylua,
 	})
 
+	utils.add_formatter_if_lsp("typescript-tools", opts.sources, {
+		null_ls.builtins.formatting.prettier,
+	})
+
+	utils.add_formatter_if_lsp("clojure-lsp", opts.sources, {
+		null_ls.builtins.diagnostics.clj_kondo,
+	})
+
+	utils.add_formatter_if_lsp("html", opts.sources, {
+		null_ls.builtins.diagnostics.tidy,
+		null_ls.builtins.formatting.djlint,
+		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.formatting.rustywind,
+	})
+
+	utils.add_formatter_if_lsp("markdown_oxide", opts.sources, {
+		null_ls.builtins.formatting.cbfmt,
+		null_ls.builtins.formatting.ocdc,
+		null_ls.builtins.formatting.remark,
+		null_ls.builtins.formatting.textlint,
+		null_ls.builtins.diagnostics.alex,
+		null_ls.builtins.diagnostics.ltrs,
+		null_ls.builtins.diagnostics.markdownlint_cli2,
+		null_ls.builtins.diagnostics.mdl,
+		null_ls.builtins.diagnostics.proselint,
+		null_ls.builtins.diagnostics.textidote,
+		null_ls.builtins.diagnostics.vale,
+		null_ls.builtins.diagnostics.write_good,
+	})
+
+	utils.add_formatter_if_lsp("phpactor", opts.sources, {
+		null_ls.builtins.formatting.phpcbf,
+		null_ls.builtins.formatting.phpcsfixer,
+		null_ls.builtins.formatting.pretty_php,
+		null_ls.builtins.diagnostics.phpstan,
+		null_ls.builtins.diagnostics.phpcs,
+		null_ls.builtins.diagnostics.phpmd,
+	})
+
+	utils.add_formatter_if_lsp("pyright", opts.sources, {
+		null_ls.builtins.formatting.black,
+		null_ls.builtins.formatting.isort,
+		null_ls.builtins.diagnostics.pylint.with({
+			diagnostics_postprocess = function(diagnostic)
+				diagnostic.code = diagnostic.message_id
+			end,
+		}),
+	})
+
+	utils.add_formatter_if_lsp("rust-analyzer", opts.sources, {
+		null_ls.builtins.formatting.dxfmt,
+		null_ls.builtins.formatting.leptosfmt,
+	})
+
+	utils.add_formatter_if_lsp("bashls", opts.sources, {
+		null_ls.builtins.formatting.shellharden,
+		null_ls.builtins.formatting.shfmt,
+		null_ls.builtins.diagnostics.dotenv_linter,
+		null_ls.builtins.hover.printenv,
+	})
+
+	utils.add_formatter_if_lsp("sqlls", opts.sources, {
+		null_ls.builtins.formatting.pg_format,
+		null_ls.builtins.formatting.sqlformat,
+		null_ls.builtins.formatting.sql_formatter,
+		null_ls.builtins.formatting.sqlfmt,
+		null_ls.builtins.formatting.sqlfluff.with({
+			extra_args = { "--dialect", "postgres" },
+		}),
+		null_ls.builtins.diagnostics.sqlfluff.with({
+			extra_args = { "--dialect", "postgres" },
+		}),
+	})
 
 	-- opts.sources = vim.list_extend(opts.sources or {}, {
 	-- C/C++, C#, Java
@@ -48,102 +126,14 @@ return function(_, opts)
 	-- Go, Javascript, Lua, Python, Typescript
 	-- null_ls.builtins.code_actions.refactoring,
 
-
-	-- Clojure
-	-- null_ls.builtins.diagnostics.clj_kondo,
-
-	-- Css
-
-	-- Dart
-	-- null_ls.builtins.formatting.dart_format,
-
 	-- Docker
 	-- null_ls.builtins.diagnostics.hadolint,
 
 	-- Editorconfig
 	-- null_ls.builtins.diagnostics.editorconfig_checker,
 
-	-- Gradle
-	-- null_ls.builtins.diagnostics.npm_groovy_lint,
-
-	-- Java
-
-	-- Javascript/Typescript
-	-- null_ls.builtins.formatting.prettier,
-
-	-- Go
-
 	-- Spell
 	-- null_ls.builtins.completion.spell,
-
-
-	-- Html
-	-- null_ls.builtins.diagnostics.tidy,
-	-- null_ls.builtins.formatting.djlint,
-	-- null_ls.builtins.formatting.prettierd,
-
-	-- html, Markdown
-	-- null_ls.builtins.formatting.rustywind,
-
-	-- Markdown
-	-- null_ls.builtins.formatting.cbfmt,
-	-- null_ls.builtins.formatting.ocdc,
-	-- null_ls.builtins.formatting.remark,
-	-- null_ls.builtins.formatting.textlint,
-	-- null_ls.builtins.diagnostics.alex,
-	-- null_ls.builtins.diagnostics.ltrs,
-	-- null_ls.builtins.diagnostics.markdownlint_cli2,
-	-- null_ls.builtins.diagnostics.mdl,
-	-- null_ls.builtins.diagnostics.proselint,
-	-- null_ls.builtins.diagnostics.textidote,
-	-- null_ls.builtins.diagnostics.vale,
-	-- null_ls.builtins.diagnostics.write_good,
-
-	-- Make
-	-- null_ls.builtins.diagnostics.checkmake,
-	-- null_ls.builtins.diagnostics.cmake_lint,
-
-
-	-- PHP
-	-- null_ls.builtins.formatting.phpcbf,
-	-- null_ls.builtins.formatting.phpcsfixer,
-	-- null_ls.builtins.formatting.pretty_php
-	-- null_ls.builtins.diagnostics.phpstan,
-	-- null_ls.builtins.diagnostics.phpcs,
-	-- null_ls.builtins.diagnostics.phpmd,
-
-	-- Python
-	-- null_ls.builtins.diagnostics.pylint.with({
-	--   diagnostics_postprocess = function(diagnostic)
-	--     diagnostic.code = diagnostic.message_id
-	--   end,
-	-- }),
-	-- null_ls.builtins.formatting.black,
-	-- null_ls.builtins.formatting.isort,
-
-	-- Lua
-
-	-- Rust
-	-- null_ls.builtins.formatting.dxfmt,
-	-- null_ls.builtins.formatting.leptosfmt,
-
-	-- Shellscript
-	-- null_ls.builtins.formatting.shellharden,
-	-- null_ls.builtins.formatting.shfmt,
-	-- null_ls.builtins.diagnostics.dotenv_linter,
-	-- null_ls.builtins.hover.printenv,
-
-	-- Sql
-	-- null_ls.builtins.formatting.pg_format,
-	-- null_ls.builtins.formatting.sqlformat,
-	-- null_ls.builtins.formatting.sql_formatter,
-	-- null_ls.builtins.formatting.sqlfmt,
-	-- null_ls.builtins.formatting.sqlfluff.with({
-	--   extra_args = { "--dialect", "postgres" }, -- change to your dialect
-	-- }),
-	-- null_ls.builtins.diagnostics.sqlfluff.with({
-	--   extra_args = { "--dialect", "postgres" }, -- change to your dialect
-	-- }),
 
 	-- Xml
 	-- null_ls.builtins.diagnostics.tidy,
