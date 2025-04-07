acf7dd5d087f288c4d8a5d3729b5d05e657ccb69
Eduardo Silva <eduardofox989@protonmail.com>
2025-04-07 17:17:42 -0300

    change: the lspconfig configuration went to config.lua and the initialization to config in init.lua
    this change was made to keep the structure of the plugins consistent, it
    didn't make sense for the initializer to be inside the config file while
    the configuration is inside the init file.

--- lua/plugins/lspconfig/config.lua
+++ lua/plugins/lspconfig/config.lua
@@ -1,20 +1,3 @@
-local lsp = require("lspconfig")
-local M = {}
-
-local servers = {
-	lua_ls = require("lsp.config.lua_ls"),
-	nil_ls = require("lsp.config.nil_ls"),
-	clangd = require("lsp.config.clangd"),
-	gopls = require("lsp.config.gopls"),
-	markdown_oxide = require("lsp.config.markdown_oxide"),
-	jsonls = require("lsp.config.jsonls"),
-	eslint = require("lsp.config.eslint"),
+return {
+	inlay_hints = { enable = true }
 }
-
-function M.setup()
-	for name, conf in pairs(servers) do
-		lsp[name].setup(conf)
-	end
-end
-
-return M
