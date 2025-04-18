5116990d2cf674ff2beb74c383fbe35a1d8cff49
Eduardo Silva <eduardofox989@protonmail.com>
2025-04-18 13:19:36 -0300

    added: keymap to run job to compile project gradle

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,3 +1,27 @@
 local config = require("plugins.nvim-jdtls.config")
+local function build_project()
+	local root = vim.fn.findfile("build.gradle", ".;")
+	if root == "" then
+		return
+	end
+	root = vim.fn.fnamemodify(root, ":p:h") -- pega só o diretório
+	vim.fn.jobstart("./gradlew classes", {
+		cwd = root,
+		stdout_buffered = true,
+		stderr_buffered = false,
+		on_stdout = function(_, data)
+			if data and data[1] ~= "" then
+				vim.notify(table.concat(data, "\n"), vim.log.levels.INFO, { title = "Gradle Build" })
+			end
+		end,
+		on_stderr = function(_, data)
+			if data and data[1] ~= "" then
+				vim.notify(table.concat(data, "\n"), vim.log.levels.ERROR, { title = "Gradle Error" })
+			end
+		end,
+	})
+end
 
-require('jdtls').start_or_attach(config)
+vim.keymap.set("n", "<leader>co", build_project, { desc = "compila projeto gradle"})
+
+require("jdtls").start_or_attach(config)
