cdcf4076d06752038e4832d6fb0aeecf84b11373
Eduardo Silva <eduardofox989@protonmail.com>
2025-04-19 12:54:12 -0300

    delete: lsp module

--- lua/lsp/capabilities.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-
-M = vim.lsp.protocol.make_client_capabilities()
-M.textDocument.completion.completionItem.snippetSupport = false
-M = require("cmp_nvim_lsp").default_capabilities(M)
-
-return M
