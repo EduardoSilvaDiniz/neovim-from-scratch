54b7f14dedb42948aa45098ca2b89d0783993734
Eduardo Silva <eduardofox989@protonmail.com>
2025-04-19 12:54:44 -0300

    remove: call lsp configs to lspconfig

--- lua/plugins/lspconfig/init.lua
+++ lua/plugins/lspconfig/init.lua
@@ -5,14 +5,7 @@ return {
 	opts = require("plugins.lspconfig.config"),
 	config = function()
 		local lsp = require("lspconfig")
-		local servers = {
-			lua_ls = require("lsp.config.lua_ls"),
-			nil_ls = require("lsp.config.nil_ls"),
-			clangd = require("lsp.config.clangd"),
-			gopls = require("lsp.config.gopls"),
-			markdown_oxide = require("lsp.config.markdown_oxide"),
-		}
-
+		local servers = {}
 		for name, config in pairs(servers) do
 			lsp[name].setup(config)
 		end
