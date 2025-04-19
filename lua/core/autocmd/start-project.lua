05ad61d71699187e26993436c760250c20b3851d
Eduardo Silva <eduardofox989@protonmail.com>
2025-04-19 16:28:31 -0300

    change: move keymaps neotree

--- lua/core/autocmd/start-project.lua
+++ lua/core/autocmd/start-project.lua
@@ -2,9 +2,7 @@ vim.api.nvim_create_autocmd("DirChanged", {
 	desc = "loading session",
 	pattern = "*",
 	callback = function()
-		local keymaps = {
-			{ "tn", "<cmd>Neotree reveal<cr>", { noremap = true } },
-		}
+		local keymaps = require("plugins.neo-tree.keymap")
 		local loadkeys = require("lib.loadkeys")
 		loadkeys.manual_load(keymaps, "neo-tree")
 	end,
