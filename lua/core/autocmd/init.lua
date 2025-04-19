a2db7419c35dbffd06cbda14ebfd6e9066c54638
Eduardo Silva <eduardofox989@protonmail.com>
2025-04-19 16:28:04 -0300

    change: rename lspconfig to start-lsp

--- lua/core/autocmd/init.lua
+++ lua/core/autocmd/init.lua
@@ -1,5 +1,5 @@
 require("core.autocmd.autosaved")
-require("core.autocmd.lspconfig")
+require("core.autocmd.start-lsp")
 require("core.autocmd.git")
 require("core.autocmd.start-project")
 require("core.autocmd.wsl-check")
--- lua/core/autocmd/start-lsp.lua
+++ lua/core/autocmd/start-lsp.lua
@@ -1,11 +1,13 @@
 vim.api.nvim_create_autocmd("BufReadPost", {
 	desc = "load keymaps to lsp",
 	pattern = "*",
-	callback = function(event)
-		local keymaps = require("plugins.lspconfig.keymap")
+	callback = function()
+		local keymaps = require("lib.lsp_keymap")
 		local loadkeys = require("lib.loadkeys")
-		loadkeys.manual_load(keymaps, "lspconfig")
+
+		loadkeys.manual_load(keymaps, nil)
 		vim.cmd("TSEnable highlight")
+
 		vim.diagnostic.config({
 			virtual_text = false,
 			virtual_lines = false,
