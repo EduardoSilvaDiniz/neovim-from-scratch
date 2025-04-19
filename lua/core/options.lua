8c4997317e7c0761673f93976b02e1b3f57670db
Eduardo Silva <eduardofox989@protonmail.com>
2025-04-19 11:40:53 -0300

    added: autocmd check at startup if it is running in wsl and make changes

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,4 +1,3 @@
-local utils = require "lib.utils"
 vim.o.background = "dark"
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
@@ -8,23 +7,7 @@ vim.g.have_nerd_font = true
 vim.wo.number = true
 vim.lsp.inlay_hint.enable(true)
 vim.opt.signcolumn = "yes:1"
-
-if utils.whoa_system("microsoft") then
-	vim.g.clipboard = {
-		name = "WslClipboard",
-		copy = {
-			["+"] = "clip.exe",
-			["*"] = "clip.exe",
-		},
-		paste = {
-			["+"] =
-			'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-			["*"] =
-			'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-		},
-		cache_enabled = false,
-	}
-end
+vim.opt.hidden = false
 
 local normal_bg = vim.api.nvim_get_hl(0, { name = "Normal" }).bg
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = normal_bg })
