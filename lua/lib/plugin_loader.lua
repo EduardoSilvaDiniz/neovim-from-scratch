0b3f9c5d3d03a6995226cd5051c115492589747f
Eduardo Silva <eduardofox989@protonmail.com>
2025-04-26 13:41:35 -0300

    refactoring: the plugin loader file is now a class

--- lua/lib/plugin_loader.lua
+++ lua/lib/plugin_loader.lua
@@ -14,73 +14,42 @@ function PluginLoader.new(dir, filter)
 	return self
 end
 
-function PluginLoader:createTable()
-	local list = self:_find_files()
-	local path_list = self:_create_list(list)
-	local plugins = self:_insert_plugins_table(path_list)
-
-	return plugins
-end
-
----@param path_list table
-function PluginLoader:_insert_plugins_table(path_list)
+function PluginLoader:create_plugins_table()
 	local plugins = {}
+	local handle = io.popen("find " .. vim.fn.stdpath("config") .. self.dir .. " -type f")
 
-	for _, path in pairs(path_list) do
-		local status, plugin = pcall(require, path)
-		if not status then
-			vim.print("not found plugin " .. path)
-		end
-		table.insert(plugins, plugin)
+	if handle == nil then
+		error("Failed to run find command")
 	end
 
+	for file in handle:lines() do
+		if self:_apply_filter(file) then
+			file = file:gmatch("%/lua%/(.+).lua$")({ 0 }):gsub("/", ".")
+
+			local status, plugin = pcall(require, file)
+
+			if not status then
+				vim.print("not found plugin " .. file)
+				break
+			else
+				table.insert(plugins, plugin)
+			end
+		end
+	end
+
+	handle:close()
 	return plugins
 end
 
----@param list table
-function PluginLoader:_create_list(list)
-	local new_list = {}
-
-	for element in list:lines() do
-		element = self:_clear_element(element)
-
-		if self.filter(element) then
-			table.insert(new_list, element)
-		end
-	end
-
-	return new_list
-end
-
-function PluginLoader:_find_files()
-	local new_list = {}
-
-	for element in pcall(io.popen,"find " .. vim.fn.stdpath("config") .. self.dir .. " -type f") do
-
-		if self.filter(element) then
-			table.insert(new_list, element)
-		end
-
-		element = self:_clear_element(element)
-	end
-
-	return new_list
-end
-
----@param element string
-function PluginLoader:_apply_filter(element)
-	for _, key in pairs(self.filter) do
-		if string.find(element, key) then
+---@param file string
+function PluginLoader:_apply_filter(file)
+	for _, key in pairs(self.filter_list) do
+		if string.find(file, key) then
 			return false
 		end
 	end
 
 	return true
-end
-
----@param element table
-function PluginLoader:_clear_element(element)
-	return element:gmatch("%/lua%/(.+).lua$")({ 0 }):gsub("/", ".")
 end
 
 return PluginLoader
