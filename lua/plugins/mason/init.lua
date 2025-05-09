bf7191780602f55f1af7fee732bf2a2dd6f67f66
Eduardo Silva <eduardofox989@protonmail.com>
2025-05-09 18:09:47 -0300

    added: mason-lspconfig, mason-null, mason-dap and list lsps, daps, fomatters to automatic installed

--- lua/plugins/mason/init.lua
+++ lua/plugins/mason/init.lua
@@ -1,4 +1,33 @@
+local config = require("plugins.mason.config")
 return {
-	"williamboman/mason.nvim",
-	opts = {}
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		opts = config.lsp,
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"mfussenegger/nvim-dap",
+		},
+		opts = config.dap,
+	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		opts = config.null,
+	},
 }
