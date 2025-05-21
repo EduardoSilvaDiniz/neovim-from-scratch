7fa48af3e436d0800e639a296b1790d7320b6eed
Eduardo Silva <eduardofox989@protonmail.com>
2025-05-21 17:56:26 -0300

    added: config to navigator plugin
    disable virtual_text and sign from code_action and code_lens_action. i
    don't like distractions

--- lua/plugins/navigator/config.lua
+++ lua/plugins/navigator/config.lua
@@ -1,1 +1,19 @@
-return {}
+return {
+	lsp = {
+		code_action = {
+			enable = true,
+			sign = false,
+			sign_priority = 40,
+			virtual_text = false,
+		},
+		code_lens_action = {
+			enable = true,
+			sign = false,
+			sign_priority = 40,
+			virtual_text = false,
+		},
+		diagnostic = {
+			virtual_text = false,
+		},
+	},
+}
