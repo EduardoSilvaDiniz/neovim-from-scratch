89d0b8a9e8404afacf1c295634a0fbd6954fac73
Eduardo Silva <eduardofox989@protonmail.com>
2025-06-11 15:29:30 -0300

    added: options to clangd

--- lsp/clangd.lua
+++ lsp/clangd.lua
@@ -2,10 +2,10 @@ local capabilities = require("lib.lsp.capabilities")
 
 return {
 	capabilities = capabilities,
-	init_options = {
-		clangdFileStatus = true,
-		usePlaceholders = true,
-		completeUnimported = true,
-		semanticHighlighting = true,
-	},
+	-- init_options = {
+	-- 	clangdFileStatus = true,
+	-- 	usePlaceholders = true,
+	-- 	completeUnimported = true,
+	-- 	semanticHighlighting = true,
+	-- },
 }
