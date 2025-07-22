58da1f1d3d6e57bfe440a9fab3b8b7e8692f017e
Eduardo Silva <eduardofox989@protonmail.com>
2025-07-22 17:31:45 -0300

    changed: js,ts,html,css,json,yaml format

--- lua/plugins/conform/config.lua
+++ lua/plugins/conform/config.lua
@@ -1,23 +1,43 @@
-local prettier_chain = { "prettierd", "prettier", stop_after_first = true }
+-- TODO typescript não tem organizador de imports
+-- ao inves disso é usando comandos do typescript-tools
+-- https://github.com/pmizio/typescript-tools.nvim
+--
+-- TSToolsOrganizeImports
+-- TSToolsSortImports
+-- TSToolsRemoveUnusedImports
+-- TSToolsRemoveUnused
+-- TSToolsAddMissingImports
+--
+-- Precisa achar alguma forma de usar os comandos do TSTools
+-- e o biome para formata
+-- biome tambem possui formatador para imports 
+-- https://biomejs.dev/pt-br/assist/actions/organize-imports/
+--
+-- source.organizeImports.biome
+--
+-- mas não faço ideia de como usar
+local biome_chain = { "biome", "biome-check", "biome-organize-imports" }
 
 return {
 	formatters_by_ft = {
 		c = { "clang-format", "cppcheck" },
 		cloujure = { "clj_kondo" },
-		css = prettier_chain,
+		css = biome_chain,
 		go = { "goimports", "gofumpt", "golines" },
-		html = prettier_chain,
+		html = biome_chain,
 		java = { "google-java-format" },
-		javascript = prettier_chain,
-		json = prettier_chain,
+		javascript = biome_chain,
+		javascriptreact = biome_chain,
+		json = biome_chain,
 		lua = { "stylua" },
 		make = { "cmakelint" },
 		nix = { "alejandra", "deadnix" },
 		rust = { "rustfmt", lsp_format = "fallback" },
 		sh = { "shfmt" },
 		sql = { "sql_formatter" },
-		typescript = prettier_chain,
-		yaml = prettier_chain,
+		typescript = biome_chain,
+		typescriptreact = biome_chain,
+		yaml = biome_chain,
 	},
 
 	formatters = {
@@ -30,6 +50,16 @@ return {
 			command = "sql-formatter",
 			args = { "-l", "sqlite", "--fix", "$FILENAME" },
 			stdin = false,
+		},
+		biome_organize = {
+			command = "biome",
+			args = {
+				"format",
+				"--organize-imports",
+				"--stdin-file-path",
+				"$FILENAME",
+			},
+			stdin = true,
 		},
 	},
 }
