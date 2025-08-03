c5c7d8c72510ff619b2faaae2ba98926baacce9d
Eduardo Silva <eduardofox989@protonmail.com>
2025-08-03 15:15:35 -0300

    remove: removed unused format

--- lua/plugins/conform/config.lua
+++ lua/plugins/conform/config.lua
@@ -1,65 +1,25 @@
--- TODO typescript não tem organizador de imports
--- ao inves disso é usando comandos do typescript-tools
--- https://github.com/pmizio/typescript-tools.nvim
---
--- TSToolsOrganizeImports
--- TSToolsSortImports
--- TSToolsRemoveUnusedImports
--- TSToolsRemoveUnused
--- TSToolsAddMissingImports
---
--- Precisa achar alguma forma de usar os comandos do TSTools
--- e o biome para formata
--- biome tambem possui formatador para imports 
--- https://biomejs.dev/pt-br/assist/actions/organize-imports/
---
--- source.organizeImports.biome
---
--- mas não faço ideia de como usar
-local biome_chain = { "biome", "biome-check", "biome-organize-imports" }
-
 return {
 	formatters_by_ft = {
-		c = { "clang-format", "cppcheck" },
-		cloujure = { "clj_kondo" },
-		css = biome_chain,
+		c = { "clang-format" },
 		go = { "goimports", "gofumpt", "golines" },
-		html = biome_chain,
-		java = { "google-java-format" },
-		javascript = biome_chain,
-		javascriptreact = biome_chain,
-		json = biome_chain,
+		java = { "google_java_format" },
 		lua = { "stylua" },
 		make = { "cmakelint" },
-		nix = { "alejandra", "deadnix" },
-		rust = { "rustfmt", lsp_format = "fallback" },
-		sh = { "shfmt" },
-		sql = { "sql_formatter" },
-		typescript = biome_chain,
-		typescriptreact = biome_chain,
-		yaml = biome_chain,
+		cmake = { "cmakelint" },
+		-- nix = { "alejandra", "deadnix" },
+		-- sql = { "sql_formatter" },
 	},
 
 	formatters = {
-		clj_kondo = {
-			command = "clj-kondo",
-			args = { "--lint" },
+		google_java_format = {
+			command = "google-java-format",
+			args = { "-i", "$FILENAME" },
 			stdin = false,
 		},
 		sql_formatter = {
 			command = "sql-formatter",
 			args = { "-l", "sqlite", "--fix", "$FILENAME" },
 			stdin = false,
-		},
-		biome_organize = {
-			command = "biome",
-			args = {
-				"format",
-				"--organize-imports",
-				"--stdin-file-path",
-				"$FILENAME",
-			},
-			stdin = true,
 		},
 	},
 }
--- lua/plugins/conform/init.lua
+++ lua/plugins/conform/init.lua
@@ -2,7 +2,7 @@ return {
 	"stevearc/conform.nvim",
 	opts = require("plugins.conform.config"),
 	config = vim.schedule(function()
-		local keymaps = require("plugins.harpoon.keymap")
+		local keymaps = require("plugins.conform.keymap")
 		local loadkeys = require("lib.loadkeys")
 		loadkeys.manual_load(keymaps, nil)
 	end),
