8b6f07200924fc61516d7933af9e275467bb433f
Eduardo Silva <eduardofox989@protonmail.com>
2025-08-03 00:38:01 -0300

    refactoring: move lsp config to file

---
+++ lua/lib/lsp/config.lua
@@ -0,0 +1,21 @@
+return {
+	severity_sort = true,
+	virtual_text = false,
+	virtual_lines = false,
+	underline = true,
+	update_in_insert = false,
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "",
+			[vim.diagnostic.severity.WARN] = "",
+			[vim.diagnostic.severity.HINT] = "",
+			[vim.diagnostic.severity.INFO] = "",
+		},
+		numhl = {
+			[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+			[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+			[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+			[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+		},
+	},
+}
