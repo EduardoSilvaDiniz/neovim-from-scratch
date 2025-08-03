7bc2853a4d93d9fe52e7e4e6a3eac3086fcc25bb
Eduardo Silva <eduardofox989@protonmail.com>
2025-08-03 00:40:16 -0300

    change: keymap harpoon

--- lua/plugins/harpoon/keymap.lua
+++ lua/plugins/harpoon/keymap.lua
@@ -1,59 +1,14 @@
 local harpoon = require("harpoon")
-for i = 1, 6 do
+for i = 1, 9 do
 	vim.keymap.set(
 		"n",
-		"<A-" .. i .. ">",
+		"h" .. i,
 		function()
 			require("harpoon"):list():select(i)
-		end,
-		{ noremap = true, silent = true }
-	)
+		end)
 end
 
 return {
-	{ "<leader>ma", function() harpoon:list():add() end,                         { desc = "Harpoon File" } },
-	{ "<leader>mm", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon Quick Menu" } },
-
-	-- {
-	-- 	"<A-1>",
-	-- 	function()
-	-- 		harpoon:list():select(1)
-	-- 	end,
-	-- 	{ noremap = true },
-	-- },
-	-- {
-	-- 	"<A-2>",
-	-- 	function()
-	-- 		harpoon:list():select(2)
-	-- 	end,
-	-- 	{ noremap = true },
-	-- },
-	-- {
-	-- 	"<A-3>",
-	-- 	function()
-	-- 		harpoon:list():select(3)
-	-- 	end,
-	-- 	{ noremap = true },
-	-- },
-	-- {
-	-- 	"<A-4>",
-	-- 	function()
-	-- 		harpoon:list():select(4)
-	-- 	end,
-	-- 	{ noremap = true },
-	-- },
-	-- {
-	-- 	"<A-5>",
-	-- 	function()
-	-- 		harpoon:list():select(5)
-	-- 	end,
-	-- 	{ noremap = true },
-	-- },
-	-- {
-	-- 	"<A-6>",
-	-- 	function()
-	-- 		harpoon:list():select(6)
-	-- 	end,
-	-- 	{ noremap = true },
-	-- },
+	{ "ha", function() harpoon:list():add() end,                         { desc = "Harpoon File" } },
+	{ "hm", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon Quick Menu" } },
 }
