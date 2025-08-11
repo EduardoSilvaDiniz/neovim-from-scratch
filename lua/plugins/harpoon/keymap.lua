13a211180ce94b35b06f6bb0f593d4d6fe53d5c9
Eduardo Silva <eduardofox989@protonmail.com>
2025-08-10 22:23:57 -0300

    change: keymap harpoon

--- lua/plugins/harpoon/keymap.lua
+++ lua/plugins/harpoon/keymap.lua
@@ -1,14 +1,23 @@
 local harpoon = require("harpoon")
 for i = 1, 9 do
-	vim.keymap.set(
-		"n",
-		"h" .. i,
-		function()
-			require("harpoon"):list():select(i)
-		end)
+	vim.keymap.set("n", "<leader>h" .. i, function()
+		require("harpoon"):list():select(i)
+	end, { noremap = true })
 end
 
 return {
-	{ "ha", function() harpoon:list():add() end,                         { desc = "Harpoon File" } },
-	{ "hm", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon Quick Menu" } },
+	{
+		"<leader>ha",
+		function()
+			harpoon:list():add()
+		end,
+		{ desc = "Harpoon File" },
+	},
+	{
+		"<leader>hm",
+		function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end,
+		{ desc = "Harpoon Quick Menu" },
+	},
 }
