7cb9bc4b6efd05183a18359b1cf265afb1e96f07
Eduardo Silva <eduardofox989@protonmail.com>
2025-12-12 21:57:57 -0300

    added laravel plugin

---
+++ lua/plugins/laravel/config.lua
@@ -0,0 +1,7 @@
+return {
+	features = {
+		pickers = {
+			provider = "telescope",
+		},
+	},
+}
---
+++ lua/plugins/laravel/init.lua
@@ -0,0 +1,16 @@
+return {
+	"adalessa/laravel.nvim",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"nvim-lua/plenary.nvim",
+		"nvim-neotest/nvim-nio",
+	},
+	cmd = { "Laravel" },
+	event = { "VeryLazy" },
+	opts = require("plugins.laravel.config"),
+	config = vim.schedule(function()
+		local keymaps = require("plugins.laravel.keymap")
+		local loadkeys = require("lib.loadkeys")
+		loadkeys.manual_load(keymaps, nil)
+	end),
+}
