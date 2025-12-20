4fd4329963d8c89312644cf2a52f8907a7dae036
Eduardo Silva <eduardofox989@protonmail.com>
2025-12-20 11:49:23 -0300

    set config to intelephense and phpactor

--- lsp/intelephense.lua
+++ lsp/intelephense.lua
@@ -1,12 +1,34 @@
 local capabilities = require("lib.lsp.capabilities")
 
+-- configuração copiada de 
+-- https://github.com/pnx/dotfiles/blob/main/nvim/lua/user/plugins/lang/php/lsp/intelephense.lua
 vim.lsp.config("intelephense", {
 	capabilities = capabilities,
 	settings = {
 		intelephense = {
+			files = {
+				maxSize = 10000000,
+				exclude = {
+					"**/.git/**",
+					"**/.svn/**",
+					"**/.hg/**",
+					"**/CVS/**",
+					"**/.DS_Store/**",
+					"**/node_modules/**",
+					"**/bower_components/**",
+					"**/vendor/**/{Tests,tests}/**",
+					"**/vendor/fakerphp/**",
+					"**/.history/**",
+					"**/vendor/**/vendor/**",
+				},
+			},
+			completion = {
+				maxItems = 40,
+			},
 			format = { enable = false },
 			folding = { enable = true },
 			environment = {
+				shortOpenTags = true,
 				includePaths = { "/usr/lib64/php8/extensions" },
 			},
 			-- possible values: stubs.txt
--- lsp/phpactor.lua
+++ lsp/phpactor.lua
@@ -1,8 +1,59 @@
-local capabilities = require("lib.lsp.capabilities")
-
+-- configuração copiada de 
+-- https://github.com/pnx/dotfiles/blob/main/nvim/lua/user/plugins/lang/php/lsp/phpactor.lua
 vim.lsp.config("phpactor", {
-	capabilities = capabilities,
-	handlers = {
-		["textDocument/publishDiagnostics"] = function() end,
+	inlay_hints = { enabled = false },
+	codelens = { enabled = false },
+	document_highlight = { enabled = false },
+	server_capabilities = {
+		definitionProvider = false,
+		declarationProvider = false,
+		renameProvider = false,
+		referencesProvider = false,
+		implementationProvider = false,
+		documentSymbolProvider = false,
+		workspaceSymbolProvider = false,
+		diagnosticProvider = nil,
+		hoverProvider = false,
+		--- @diagnostic disable-next-line: assign-type-mismatch
+		completionProvider = false,
+	},
+	init_options = {
+		-- Only interested in code actions. Disable everything else!
+		["completion_worse.completor.doctrine_annotation.enabled"] = false,
+		["completion_worse.completor.imported_names.enabled"] = false,
+		["completion_worse.completor.worse_parameter.enabled"] = false,
+		["completion_worse.completor.named_parameter.enabled"] = false,
+		["completion_worse.completor.constructor.enabled"] = false,
+		["completion_worse.completor.class_member.enabled"] = false,
+		["completion_worse.completor.scf_class.enabled"] = false,
+		["completion_worse.completor.local_variable.enabled"] = false,
+		["completion_worse.completor.subscript.enabled"] = false,
+		["completion_worse.completor.declared_function.enabled"] = false,
+		["completion_worse.completor.declared_constant.enabled"] = false,
+		["completion_worse.completor.declared_class.enabled"] = false,
+		["completion_worse.completor.expression_name_search.enabled"] = false,
+		["completion_worse.completor.use.enabled"] = false,
+		["completion_worse.completor.class_like.enabled"] = false,
+		["completion_worse.completor.type.enabled"] = false,
+		["completion_worse.completor.keyword.enabled"] = false,
+		["completion_worse.completor.docblock.enabled"] = false,
+		["completion_worse.completor.attribute.enabled"] = false,
+		["completion_worse.completor.constant.enabled"] = false,
+		["completion_worse.snippets"] = false,
+		["completion.limit"] = 0,
+		-- Disable push diagnostics
+		["language_server.diagnostics_on_open"] = false,
+		["language_server.diagnostics_on_save"] = false,
+		["language_server.diagnostics_on_update"] = false,
+		["language_server_worse_reflection.diagnostics.enable"] = false,
+		["language_server_worse_reflection.inlay_hints.enable"] = false,
+		["language_server_php_cs_fixer.enabled"] = false,
+		["language_server_phpstan.enabled"] = false,
+		["language_server_psalm.enabled"] = false,
+		["code_transform.refactor.generate_accessor.prefix"] = "get",
+		["code_transform.refactor.generate_accessor.upper_case_first"] = true,
+		["code_transform.refactor.generate_mutator.prefix"] = "set",
+		["code_transform.refactor.generate_mutator.upper_case_first"] = true,
+		["code_transform.refactor.generate_mutator.fluent"] = true,
 	},
 })
