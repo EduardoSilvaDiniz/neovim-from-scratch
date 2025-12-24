d8dd46ef6614f7ea86fe668d89278b5738bd54fe
Eduardo Silva <eduardofox989@protonmail.com>
2025-12-23 23:16:27 -0300

    fix treesitter config

--- lua/plugins/treesitter/init.lua
+++ lua/plugins/treesitter/init.lua
@@ -2,12 +2,12 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
-		"nvim-treesitter/nvim-treesitter-textobjects",
 		{
 			"hiphish/rainbow-delimiters.nvim",
 			event = "VeryLazy",
 		},
 	},
+	lazy = false,
 	build = ":TSUpdate",
 	config = require("plugins.treesitter.config"),
 }
