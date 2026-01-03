9a2ae8f2a386c91666d725fc2b196e682142310d
Eduardo Silva <eduardofox989@protonmail.com>
2026-01-03 16:17:06 -0300

    added: keymaps to navigation buffers

--- init.lua
+++ init.lua
@@ -3,6 +3,10 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.softtabstop = 2
+vim.opt.autoindent = true
+vim.opt.smartindent = true
 vim.g.have_nerd_font = true
 vim.g.editorconfig = true
 vim.wo.number = true
@@ -15,28 +19,29 @@ vim.opt.signcolumn = "yes:1"
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-
 require("config.lazy")
 require("config.autocmds")
 
-vim.lsp.enable('lua_ls')
-vim.lsp.enable('vue_ls')
-vim.lsp.enable('vtsls')
-vim.lsp.enable('tailwindcss')
-vim.lsp.enable('intelephense')
-vim.lsp.enable('phpactor')
-vim.lsp.enable('emmet_language_server')
-vim.lsp.enable('laravel-ls')
+vim.lsp.enable("lua_ls")
+vim.lsp.enable("vue_ls")
+vim.lsp.enable("vtsls")
+vim.lsp.enable("tailwindcss")
+vim.lsp.enable("intelephense")
+vim.lsp.enable("phpactor")
+vim.lsp.enable("emmet_language_server")
+vim.lsp.enable("laravel_ls")
 
-vim.keymap.set(
-  { "n", "v" },
-  "ga",
-  vim.lsp.buf.code_action,
-  { desc = "LSP Code Action" }
-)
+vim.keymap.set({ "n", "v" }, "ga", vim.lsp.buf.code_action, { desc = "LSP Code Action" })
+vim.keymap.set("n", "<leader>wh", "<C-w>h", { silent = true })
+vim.keymap.set("n", "<leader>wl", "<C-w>l", { silent = true })
+vim.keymap.set("n", "<leader>wj", "<C-w>j", { silent = true })
+vim.keymap.set("n", "<leader>wk", "<C-w>k", { silent = true })
 
+vim.keymap.set("n", "<space>x", ":.lua<CR>", { silent = true })
 
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { '<filetype>' },
-  callback = function() vim.treesitter.start() end,
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "<filetype>" },
+	callback = function()
+		vim.treesitter.start()
+	end,
 })
