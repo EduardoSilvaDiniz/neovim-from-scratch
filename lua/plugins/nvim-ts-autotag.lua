8dbf20c06554d7db11dbaf1b93abf44f5478697a
Eduardo Silva <eduardofox989@protonmail.com>
2026-01-03 16:12:28 -0300

    updated: nvim ts autotag

--- lua/plugins/nvim-ts-autotag.lua
+++ lua/plugins/nvim-ts-autotag.lua
@@ -1,13 +1,12 @@
 return {
-"windwp/nvim-ts-autotag",
-    dependencies = {'nvim-treesitter/nvim-treesitter'},
-config = function ()
-	require('nvim-ts-autotag').setup({
-  opts = {
-    enable_close = true,
-    enable_rename = true,
-    enable_close_on_slash = true
-  },
-})
-end
+	"windwp/nvim-ts-autotag",
+	lazy = false,
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	opts = {
+		opts = {
+			enable_close = true,
+			enable_rename = true,
+			enable_close_on_slash = true,
+		},
+	},
 }
