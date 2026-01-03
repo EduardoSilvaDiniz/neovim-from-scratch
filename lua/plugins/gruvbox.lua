5c07a3527ff8c2a24df52d3954b6c238f0315e41
Eduardo Silva <eduardofox989@protonmail.com>
2026-01-03 16:13:21 -0300

    enabled neotree and fix color

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -4,5 +4,11 @@ return {
 	priority = 1000,
 	config = function()
 		vim.cmd.colorscheme("gruvbox-material")
+
+		vim.api.nvim_set_hl(0, "NeoTreeNormal", { link = "Normal" })
+		vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { link = "Normal" })
+		vim.api.nvim_set_hl(0, "NeoTreeFloatBorder", { link = "Normal" })
+		vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = vim.api.nvim_get_hl_by_name("Normal", true).background })
+		vim.api.nvim_set_hl(0, "NeoTreeCursorLine", { bg = CursorLine })
 	end,
 }
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,3 @@
-if true then
-	return {}
-end
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v3.x",
@@ -10,60 +7,76 @@ return {
 		"MunifTanjim/nui.nvim",
 		"nvim-tree/nvim-web-devicons",
 	},
+	keys = {
+		{ "<leader>e", "<Cmd>Neotree<CR>", desc = "Abrir Neo-tree" },
+	},
 	opts = {
-	window = {
-		position = "float",
-		popup = {
-			border = "rounded",
-			title = "Neo-Tree",
-			title_pos = "center",
+		-- window = {
+		-- 	position = "float",
+		-- 	popup = {
+		-- 		border = "rounded",
+		-- 		title = "Neo-Tree",
+		-- 		title_pos = "center",
+		-- 	},
+		-- },
+		event_handlers = {
+			{
+				event = "file_open_requested",
+				handler = function()
+					require("neo-tree.command").execute({ action = "close" })
+				end,
+			},
+			{
+				event = "file_renamed",
+				handler = function(data)
+					Snacks.rename.on_rename_file(data.source, data.destination)
+				end,
+			},
+			{
+				event = "file_moved",
+				handler = function(data)
+					Snacks.rename.on_rename_file(data.source, data.destination)
+				end,
+			},
 		},
-	},
-	event_handlers = {
-		{
-			event = "file_open_requested",
-			handler = function()
-				require("neo-tree.command").execute({ action = "close" })
-			end,
+		default_component_configs = {
+			indent = { padding = 1 },
 		},
-	},
-	filesystem = {
-		group_empty_dirs = true,
-		filtered_items = {
-			visible = true,
-		},
-		window = {
-			mappings = {
-				["<C-'>"] = "close_window",
-				["\\"] = "close_window",
-				["l"] = "open",
-				["h"] = "back_close_folder",
-				["<cr>"] = "refresh",
-				["P"] = {
-					"toggle_preview",
-					config = {
-						use_float = true,
-						use_image_nvim = true,
-						title = "Neo-tree Preview",
+		reveal_current_file = true,
+		filesystem = {
+			follow_current_file = true,
+			use_libuv_file_watcher = true,
+			group_empty_dirs = true,
+			filtered_items = {
+				visible = true,
+			},
+			window = {
+				mappings = {
+					["<C-'>"] = "close_window",
+					["\\"] = "close_window",
+					["l"] = "open",
+					["h"] = "back_close_folder",
+					["<cr>"] = "refresh",
+					["P"] = {
+						"toggle_preview",
+						config = {
+							use_float = true,
+							use_image_nvim = true,
+							title = "Neo-tree Preview",
+						},
 					},
 				},
 			},
 		},
+		commands = {
+			back_close_folder = function(state)
+				local node = state.tree:get_node()
+				if node.type == "directory" and node:is_expanded() then
+					state.commands.toggle_node(state)
+				else
+					require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+				end
+			end,
+		},
 	},
-	commands = {
-		back_close_folder = function(state)
-			local node = state.tree:get_node()
-			if node.type == "directory" and node:is_expanded() then
-				state.commands.toggle_node(state)
-			else
-				require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
-			end
-		end,
-	},
-	},
-	config = function()
-	vim.keymap.set("n", "<leader>e", "<Cmd>Neotree<CR>")
-	end
-
 }
-
