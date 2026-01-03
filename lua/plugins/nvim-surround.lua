d06ceb2dadb8263068d57a131e6c3d0f87561818
Eduardo Silva <eduardofox989@protonmail.com>
2026-01-03 16:11:56 -0300

    added: surround

---
+++ lua/plugins/nvim-surround.lua
@@ -0,0 +1,8 @@
+return {
+	"kylechui/nvim-surround",
+	version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+	event = "VeryLazy",
+	config = function()
+		require("nvim-surround").setup({})
+	end,
+}
