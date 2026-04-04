84c4b53a3d5472580fb7c02939132a212599eb74
Eduardo Silva <eduardofox989@protonmail.com>
2026-04-04 12:45:09 -0300

    removi autocmds desnecessarios

--- lua/core/autocmd/start-project.lua
+++
@@ -1,9 +0,0 @@
-vim.api.nvim_create_autocmd("DirChanged", {
-	desc = "loading session",
-	pattern = "*",
-	callback = function()
-		local keymaps = require("plugins.neo-tree.keymap")
-		local loadkeys = require("lib.loadkeys")
-		loadkeys.manual_load(keymaps, "neo-tree")
-	end,
-})
